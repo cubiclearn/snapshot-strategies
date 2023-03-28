80de33551a53d964f99d205cfa9fb4eb8810def9
lucadonnoh <donnoh99@gmail.com>
2023-03-28 14:05:36 +0200

    feat: initial karma strategy

---
+++ src/strategies/karma-quadratic-rating-of/README.md
@@ -0,0 +1,13 @@
+# erc20-balance-of
+
+Quadratic strategy for voting power based on karma.
+
+Here is an example of parameters:
+
+```json
+{
+  "address": "0x...",
+  "symbol": "...",
+  "decimals": 0
+}
+```
---
+++ src/strategies/karma-quadratic-rating-of/index.ts
@@ -0,0 +1,34 @@
+import { BigNumberish } from '@ethersproject/bignumber';
+import { formatUnits } from '@ethersproject/units';
+import { Multicaller } from '../../utils';
+
+export const author = 'Decentralized Autonomous Education (DAE)';
+export const version = '0.1.0';
+
+const abi = [
+  'function ratingOf(address account) external view returns (uint64)'
+];
+
+export async function strategy(
+  space,
+  network,
+  provider,
+  addresses,
+  options,
+  snapshot
+): Promise<Record<string, number>> {
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+
+  const multi = new Multicaller(network, provider, abi, { blockTag });
+  addresses.forEach((address) =>
+    multi.call(address, options.address, 'ratingOf', [address])
+  );
+  const result: Record<string, BigNumberish> = await multi.execute();
+
+  return Object.fromEntries(
+    Object.entries(result).map(([address, balance]) => [
+      address,
+      Math.sqrt(parseFloat(formatUnits(balance, options.decimals)))
+    ])
+  );
+}
